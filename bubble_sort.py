cc75a49f5d11147b2677513f6ee4dcc338128850
Ivan Vucetic <vuceta87@yahoo.com>
2015-08-21 11:37:41 +0200

    stops sorting if no swaps in last pass

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,15 +1,21 @@
 def bubbleSort(list):
     k = 1
     while k < len(list):
+        swap = 0
         for i in range(len(list)-k):
             if list[i] > list[i+1]:
                 temp = list[i]
                 list[i] = list[i+1]
                 list[i+1] = temp
-        print list
+                swap += 1
+
         k += 1
+        print list, swap
+        if swap == 0: break
+        	
+        
 
 
-a = [2, 1, 6, 4, 3]
+a = [2, 1, 6, 4, 3, 7, 1, 2, 2, 9, 5]
 
 bubbleSort(a)        
